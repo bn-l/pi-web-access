619426eec861a4096ff3cf84e7e3a9f07be19664
bn-l <13162197+bn-l@users.noreply.github.com>
2026-06-29 13:37:38 +1000

    fix: remove XDG_CONFIG_HOME, use ~/.pi/agent like pi core

--- utils.ts
+++ utils.ts
@@ -3,8 +3,7 @@ import { join } from "node:path";
 
 export function getWebSearchConfigDir(): string {
 	if (process.env.PI_CODING_AGENT_DIR) return process.env.PI_CODING_AGENT_DIR;
-	if (process.env.XDG_CONFIG_HOME) return join(process.env.XDG_CONFIG_HOME, "pi");
-	return join(homedir(), ".pi");
+	return join(homedir(), ".pi", "agent");
 }
 
 export function getWebSearchConfigPath(): string {
